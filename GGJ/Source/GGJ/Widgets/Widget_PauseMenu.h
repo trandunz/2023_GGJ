cc99ac337fa321883d671465993e435e7f895b3c
reeeeeee <trandunz@gmail.com>
2023-02-04 16:49:05 +1300

    +PauseMenu Widget

--- GGJ/Source/GGJ/Widgets/Widget_PauseMenu.h
+++ GGJ/Source/GGJ/Widgets/Widget_PauseMenu.h
@@ -13,5 +13,28 @@ UCLASS()
 class GGJ_API UWidget_PauseMenu : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Buttons, meta = (BindWidget))
+	class UButton* Resume_Button;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Buttons, meta = (BindWidget))
+	class UButton* Home_Button;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Buttons, meta = (BindWidget))
+	class UButton* Quit_Button;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= loadName)
+	FName LevelLoad_Name;
+
+	UFUNCTION()
+	void HideWidget(bool _hideState);
+	
+private:
+	void ResumeGame();
+	void ChangeScene();
+	void QuitApplication();
+
+protected:
+	virtual void NativeOnInitialized() override;
 	
 };
