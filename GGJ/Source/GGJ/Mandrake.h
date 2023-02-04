d7e0b30d3d03324c9d872ac972ae98b76f718ce9
big pp <54924605+trandunz@users.noreply.github.com>
2023-02-04 18:23:30 +1300

    + Screaming Mandrake

--- GGJ/Source/GGJ/Mandrake.h
+++ GGJ/Source/GGJ/Mandrake.h
@@ -3,15 +3,15 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Interfaces/VegetableInterface.h"
-#include "Carrot.generated.h"
+#include "Mandrake.generated.h"
 
 UCLASS()
-class GGJ_API ACarrot : public AActor, public IVegetableInterface
+class GGJ_API AMandrake : public AActor, public IVegetableInterface
 {
 	GENERATED_BODY()
 	
 public:	
-	ACarrot();
+	AMandrake();
 
 protected:
 	virtual void BeginPlay() override;
@@ -19,11 +19,15 @@ protected:
 public:	
 	virtual void Tick(float DeltaTime) override;
 
-	void SetSimulatePhysics(bool _simulatePhysics);
-
 	virtual void Throw(FVector _direction) override;
 	virtual void Harvest() override;
+	
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Mesh, meta = (AllowPrivateAccess))
 	class UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Audio, meta = (AllowPrivateAccess))
+	class USoundWave* ScreamSound;
+
+	bool IsHarvested{};
 };
